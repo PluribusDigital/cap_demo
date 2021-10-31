2b41696aba3b34c2e707f60071a5960bf5799689
Camille Clayton <camclay@users.noreply.github.com>
2021-10-31 15:47:51 -0400

    Update CAP-GetCurrentAgent.py
    Updates for load speed

--- v1/CAP-GetCurrentAgent.py
+++ v1/CAP-GetCurrentAgent.py
@@ -6,11 +6,11 @@
 # Program gets a file from S3
 # Parses File for Phone Number
 # Passes Phone Number to AWS Connect
-# This code needs to execute in under 20 seconds, size of DynamoDB Table and Lambda Warm Up time impact this
+# This code needs to execute in under 8 seconds, size of DynamoDB Table and Lambda Warm Up time impact this
 # Colocate the AWS region where your Connect and Lambda will exist.
-import boto3
-import json
 import os
+import json
+import boto3
 
 def lambda_handler(event, context):
     try:
@@ -54,16 +54,16 @@ def lambda_handler(event, context):
         agentPhoneNum = agent_entry[1]
     else:
         print("Debug: No agent found")
-        ## You can either set a default here for calling a contact or update testReturn as error value
+        ## You can either set a default here for calling a contact or update testReturn as an error value
         ## agentName = "DefaultAgent"
         ## agentPhoneNum = "+1-555-555-5555"
         ## Phone Number return format requires dashes, when using the GUI it requires no dashes, bug as of 06/20/2020
 
     ## This can be used as a piece of helper code, add a debug var in the testReturn, and its output will be encoded into the CloudWatch logs.
-    try:
-        debug = type(event)
-    except:
-        debug = 'IssueWithTypeToString'
+    #try:
+    #    debug = type(event)
+    #except:
+    #    debug = 'IssueWithTypeToString'
     
     #testReturn = {'Name': agentName, 'Address': agentPhoneNum,'Role': requestedRole, 'Debug': debug}
     testReturn = {'Name': agentName, 'Address': agentPhoneNum,'Role': requestedRole}
